bb2e9d71e71f6ac291740c97c990351f3c4c7a47
Eugen Fedorin <2281502@ukr.net>
2023-04-23 16:18:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ let item = "";
 let textareaElement = document.getElementsByName('textdesc')[0];
 let form = document.getElementById("form");
 
+let submitBtn = document.getElementById("submitBtn");
+
+
+submitBtn.addEventListener("click", function (){
+    item = textareaElement.value;
+    tg.MainButton.setText("Відправити...")
+    tg.MainButton.show();
+})
+
+
 form.addEventListener("click", function (){
 
     item = textareaElement.value;
@@ -19,3 +29,4 @@ Telegram.WebApp.onEvent("mainButtonClicked", function (){
     tg.sendData(item);
 });
 
+// textareaElement = `${tg.initDataUnsafe.text}`

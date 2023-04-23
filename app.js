9901a155981a5df2b775b6e06592f91058e844b1
Eugen Fedorin <2281502@ukr.net>
2023-04-23 18:01:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.text = "Send"; //изменяем текст кнопки
+tg.MainButton.setText("Send1"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
 
 let submitBtn = document.getElementById("submitBtn");
+let text =  document.getElementById("textDesc");
 
 submitBtn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
     if (tg.MainButton.isVisible){ //если кнопка показана
@@ -16,4 +18,10 @@ submitBtn.addEventListener('click', function(){ //вешаем событие н
     else{ //иначе
         tg.MainButton.show() //показываем
     }
+});
+
+tg.WebApp.onEvent('mainButtonClicked', function(){
+    
+    tg.sendData(text.value);
+    //при клике на основную кнопку отправляем данные в строковом виде
 });

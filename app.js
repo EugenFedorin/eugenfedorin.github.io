b09e43c269baa979ef74819036f8ba46c3a5875d
Eugen Fedorin <2281502@ukr.net>
2023-04-23 17:42:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "2cab37";
-
-let item = "";
-let textareaElement = document.getElementsByName('textdesc')[0];
-let form = document.getElementById("form");
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
 
 let submitBtn = document.getElementById("submitBtn");
 
-
-submitBtn.addEventListener("click", function (){
-    item = textareaElement.value;
-    tg.MainButton.setText("Відправити...")
-    tg.MainButton.show();
-})
-
-
-form.addEventListener("click", function (){
-
-    item = textareaElement.value;
-
+submitBtn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+    if (tg.MainButton.isVisible){ //если кнопка показана
+        tg.MainButton.hide() //скрываем кнопку
+    }
+    else{ //иначе
+        tg.MainButton.show() //показываем
+    }
 });
-
-Telegram.WebApp.onEvent("mainButtonClicked", function (){
-    tg.sendData(item);
-});
-
-// textareaElement = `${tg.initDataUnsafe.text}`
